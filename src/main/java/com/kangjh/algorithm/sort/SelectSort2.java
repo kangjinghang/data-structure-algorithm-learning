73c344ee7465a6fa801020108b170cacf7995b76
kangjinghang <34730988+kangjinghang@users.noreply.github.com>
2020-08-09 23:54:21 +0800

    init2

--- src/main/java/com/kangjh/algorithm/sort/SelectSort2.java
+++ src/main/java/com/kangjh/algorithm/sort/SelectSort2.java
@@ -1,5 +1,7 @@
 package com.kangjh.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * SelectSort2
  *
@@ -12,7 +14,21 @@ public class SelectSort2 {
     public static void main(String[] args) {
         int[] arr = {101, 34, 119, 1};
         for (int i = 0; i < arr.length - 1; i++) {
-            
+            int minIndex = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            // 将最小值，放在arr[i], 即交换
+            if (minIndex != i) {
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+            System.out.println("第" + (i + 1) + "轮后~~");
+            System.out.println(Arrays.toString(arr));
         }
     }
 
